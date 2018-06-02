cf0680ae6dc654872d766678a87de21ac98570bc
Tomasz Radziejewski <tomasz@radziejewski.pl>
2018-06-02 13:07:59 +0200

    Setup backend CORS

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const config = require('./config');
 const graphqlHTPP = require('express-graphql');
+const cors = require('cors');
+
 const schema = require('./graphql/schema');
 
 const app = express();
+
+app.use(cors()) // enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
 
 app.use('/', graphqlHTPP({
   schema,
